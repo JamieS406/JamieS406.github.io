96b4dd4727abe556b574706bd655d4ba8c6bca50
JamieS406 <thisismyjam24601@gmail.com>
2025-04-28 21:19:43 +1000

    timestamp success step 1-4

--- assignment2/script.js
+++ assignment2/script.js
@@ -33,4 +33,42 @@ function OpenPip() {
     document.exitPictureInPicture();
   }
 }
+
 //Ask if it's okay if I change the playhead thing so i can have timestamps
+
+//Attempting timestamping
+const step1Button = document.querySelector("#stepOne");
+
+step1Button.addEventListener("click", oneStamp);
+console.log(stepOne);
+
+function oneStamp() {
+  video.currentTime = 29;
+}
+
+const step2Button = document.querySelector(".stepTwo");
+
+step2Button.addEventListener("click", twoStamp);
+console.log(stepTwo);
+
+function twoStamp() {
+  video.currentTime = 42;
+}
+
+const step3Button = document.querySelector("#stepThree");
+
+step3Button.addEventListener("click", threeStamp);
+console.log(stepThree);
+
+function threeStamp() {
+  video.currentTime = 53;
+}
+
+const step4Button = document.querySelector("#stepFour");
+
+step4Button.addEventListener("click", fourStamp);
+console.log(stepFour);
+
+function fourStamp() {
+  video.currentTime = 81;
+}
