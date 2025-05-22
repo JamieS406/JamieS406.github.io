5f4e2a9f15cffa46e043fe31acc20a533193fe4c
JamieS406 <thisismyjam24601@gmail.com>
2025-05-22 12:18:13 +1000

    fix bg issues preliminary, do part of navi control, edit classes and styling

--- DMS3_Website/script.js
+++ DMS3_Website/script.js
@@ -12,7 +12,7 @@ document.addEventListener("mousemove", function (e) {
     const moveY = (e.clientY - window.innerHeight / 2) / 30;
 
     blob.style.transform = `translate(${moveX}px, ${moveY}px)`;
-    oval1.style.transform = `rotate(-10deg) translate(${moveX * 0.5}px, ${
+    oval1.style.transform = `rotate(10deg) translate(${moveX * 0.5}px, ${
       moveY * 0.3
     }px)`;
     oval2.style.transform = `rotate(5deg) translate(${moveX * -0.3}px, ${
