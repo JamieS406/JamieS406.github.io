420098c0f752583e322c60c1807a143c15449fa1
JamieS406 <thisismyjam24601@gmail.com>
2025-06-02 23:30:32 +1000

    annotations 1

--- assignment3/script.js
+++ assignment3/script.js
@@ -8,13 +8,21 @@ let count = 0;
 function moveBell() {
   bell.classList.add("bellanimation");
 
-  //   if (count === 0) {
-  //     console.log("movebell");
-  //     bell.classList.add("bellanimation");
-  //     count = 1;
-  //   } else {
-  //     bell.classList.remove("bellanimation");
-  //     count = 1;
-  //   }
+  // if (!clicked) {
+  //   bell.classList.add("bellanimation");
+  //   clicked = true;
+  // } else {
+  //   myInput.style.translate = "0px 0px";
+  //   clicked = false;
+  // }
+
+  // if (count === 0) {
+  //   console.log("movebell");
+  //   bell.classList.add("bellanimation");
+  //   count = 1;
+  // } else {
+  //   bell.classList.add("bellanimation");
+  //   count = 2;
+  // }
   //   bell.style.transform = "translateY(-300px)";
 }
